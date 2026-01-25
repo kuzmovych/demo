a8e1e377c47269a9836862b802bd9dd81e9a8ecf
Max Kuzmovych <kuzmovychm@gmail.com>
2026-01-25 18:46:18 +0100

    add empty transactional.outbox

---
+++ transactional.outbox/src/test/java/com/kuzmovych/transactional/outbox/TransactionalOutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmovych.transactional.outbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalOutboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
