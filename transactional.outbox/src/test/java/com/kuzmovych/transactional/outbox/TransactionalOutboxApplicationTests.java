c63908afcdd32d5168f0165a93fc43d3863cb07e
Max Kuzmovych <kuzmovychm@gmail.com>
2026-03-09 00:18:03 +0100

    add testcontainers, fix context load test

--- transactional.outbox/src/test/java/com/kuzmovych/transactional/outbox/TransactionalOutboxApplicationTests.java
+++ transactional.outbox/src/test/java/com/kuzmovych/transactional/outbox/TransactionalOutboxApplicationTests.java
@@ -2,9 +2,35 @@ package com.kuzmovych.transactional.outbox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class TransactionalOutboxApplicationTests {
+
+	@Container
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.1");
+
+	@Container
+	static final RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:4.2-management");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+
+		registry.add("spring.rabbitmq.host", rabbitmq::getHost);
+		registry.add("spring.rabbitmq.port", rabbitmq::getAmqpPort);
+		registry.add("spring.rabbitmq.virtual-host", () -> "/");
+		registry.add("spring.rabbitmq.username", () -> "guest");
+		registry.add("spring.rabbitmq.password", () -> "guest");
+	}
 
 	@Test
 	void contextLoads() {
